15623d03aca5475ed571d6e6e4e94558bccef4f3
gonzalolhdev <gonzalolhdev@gmail.com>
2022-06-01 21:13:28 -0300

    complete app, missing tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/App.scss";
+import { Route, Routes } from "react-router-dom";
+import Search from "./Pages/Search";
+import Detail from "./Pages/Detail";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
